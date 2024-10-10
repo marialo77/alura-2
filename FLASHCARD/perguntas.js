5c69dd6c5020fea33a7921b9ca371f19a34c88d9
marialo77 <jorgereginaluiza@gmail.com>
2024-10-10 08:10:51 -0300

    Add files via upload

---
+++ FLASHCARD/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'comida',
+    'QUAL A MELHOR COMIDA?',
+    'BATATA FRITA'
+)
+
+criaCartao(
+    'PROFISSÃO',
+    'QUAL SUA FUTURA PROFISSÃO?',
+    'FARMACIA'
+)
+
+criaCartao(
+    'CURIOSIDADE',
+    'QUAL O ESTILO MUSICAL MAIS ESCUTADO?',
+    'HIP HOP'
+)
+
+criaCartao(
+    'FATOS',
+    'POR QUE MATEMATICA E DIFICIL?',
+    'NINGUEM GOSTA DE NUMEROS'
+)
